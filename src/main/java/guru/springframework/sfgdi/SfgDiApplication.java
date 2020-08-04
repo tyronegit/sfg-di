09950de8a1798262b01918dfabe0d1020cad388f
tyronegit <tatatkins@gmail.com>
2020-08-04 16:09:44 -0400

    upto lesson 38

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,39 @@
+package guru.springframework.sfgdi;
+/*
+ * Created by ta 8/1/2020.
+ */
+
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
+import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController"); // Spring is providing a instance of the bean controller.
+		                                                                           // The framework is doing all of the management.
+		System.out.println("---- Primary Bean ----");
+		System.out.println(myController.sayHello());
+
+		System.out.println("---- Property ----");  // lest preferred method
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("---- Setter ----"); // medium preferred method
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("---- Constructor ----"); //preferred Injection method
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
+	}
+
+
+}
